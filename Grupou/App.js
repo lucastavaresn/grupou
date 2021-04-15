84828c3dda888cba071173d744b5b4d85e37751e
qvital <qvital@icatuseguros.com.br>
2021-04-14 22:13:09 -0300

    limpando head

--- Grupou/App.js
+++ Grupou/App.js
@@ -1,8 +1,6 @@
 import React, {} from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import Pergunta from './pages/Pergunta/Perguntas';
-import Pergunta_Resultado from './pages/Pergunta/Perguntas_Resultado';
-
 
 export default function App() {
   return (
